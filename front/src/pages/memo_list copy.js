ff6f5de9ac79df49a80bf5cc796474f6f483c5df
TABLET-MSU7MS1B\User <Shironowaru@icloud.com>
2024-09-14 14:55:46 +0900

    全て追加しました

---
+++ front/src/pages/memo_list copy.js
@@ -0,0 +1,9 @@
+
+export default function MemoLink() {
+    return (
+      <div style={{ fontFamily: 'GenSenMaruGothic, sans-serif' }}>
+        <h1>シェアっと！</h1>
+        <p>メッセージ</p>
+      </div>
+    );
+  }
